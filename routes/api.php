64b49ef9c8a29f2b3047db65104c4823c0f9336c
2021 DSM41 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-12-04 13:49:11 -0600

    Post table and api route user/posts

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,8 @@ Route::middleware('auth:sanctum')->get('auth/user', function (Request $request) 
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->get('/user/posts', function (Request $request) {
+    return $request->user()->posts;
+});
+
 Route::post('/auth/token',[TokenController::class,'store']);
